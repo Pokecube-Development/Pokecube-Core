da801cfe9763a5d2ba6d0759e89af1de528d70ab
Thutmose <elpatricimo@gmail.com>
2018-08-20 02:32:14 -0400

    cleanup and import

--- src/main/java/pokecube/core/blocks/healtable/BlockHealTable.java
+++ src/main/java/pokecube/core/blocks/healtable/BlockHealTable.java
@@ -14,7 +14,6 @@ import net.minecraft.entity.item.EntityItem;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.inventory.IInventory;
 import net.minecraft.item.ItemStack;
-import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
